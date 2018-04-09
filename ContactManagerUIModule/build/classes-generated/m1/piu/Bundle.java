5542e369529b32aa454ff55e28aa6188c00a7eef
nicolas <nico.duret@orange.fr>
2018-04-09 15:31:52 +0200

    partie 4

---
+++ ContactManagerUIModule/build/classes-generated/m1/piu/Bundle.java
@@ -0,0 +1,146 @@
+package m1.piu;
+/** Localizable strings for {@link m1.piu}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>clear</i>
+     * @see Clear
+     */
+    static String CTL_Clear() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_Clear");
+    }
+    /**
+     * @return <i>addressBook</i>
+     * @see addressBookTopComponent
+     */
+    static String CTL_addressBookAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_addressBookAction");
+    }
+    /**
+     * @return <i>addressBook Window</i>
+     * @see addressBookTopComponent
+     */
+    static String CTL_addressBookTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_addressBookTopComponent");
+    }
+    /**
+     * @return <i>adressBook</i>
+     * @see adressBookTopComponent
+     */
+    static String CTL_adressBookAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_adressBookAction");
+    }
+    /**
+     * @return <i>adressBook Window</i>
+     * @see adressBookTopComponent
+     */
+    static String CTL_adressBookTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_adressBookTopComponent");
+    }
+    /**
+     * @return <i>contactDetails</i>
+     * @see contactDetailsTopComponent
+     */
+    static String CTL_contactDetailsAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_contactDetailsAction");
+    }
+    /**
+     * @return <i>contactDetails Window</i>
+     * @see contactDetailsTopComponent
+     */
+    static String CTL_contactDetailsTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_contactDetailsTopComponent");
+    }
+    /**
+     * @return <i>contactList</i>
+     * @see contactListTopComponent
+     */
+    static String CTL_contactListAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_contactListAction");
+    }
+    /**
+     * @return <i>contactList Window</i>
+     * @see contactListTopComponent
+     */
+    static String CTL_contactListTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_contactListTopComponent");
+    }
+    /**
+     * @return <i>listContact</i>
+     * @see listContactTopComponent
+     */
+    static String CTL_listContactAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_listContactAction");
+    }
+    /**
+     * @return <i>listContact Window</i>
+     * @see listContactTopComponent
+     */
+    static String CTL_listContactTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_listContactTopComponent");
+    }
+    /**
+     * @return <i>test</i>
+     * @see listTopComponent
+     */
+    static String CTL_testAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_testAction");
+    }
+    /**
+     * @return <i>test Window</i>
+     * @see listTopComponent
+     */
+    static String CTL_testTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_testTopComponent");
+    }
+    /**
+     * @return <i>Contact Form</i>
+     * @see ContactFormWizardIterator
+     */
+    static String ContactFormWizardIterator_displayName() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "ContactFormWizardIterator_displayName");
+    }
+    /**
+     * @return <i>This is a addressBook window</i>
+     * @see addressBookTopComponent
+     */
+    static String HINT_addressBookTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_addressBookTopComponent");
+    }
+    /**
+     * @return <i>This is a adressBook window</i>
+     * @see adressBookTopComponent
+     */
+    static String HINT_adressBookTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_adressBookTopComponent");
+    }
+    /**
+     * @return <i>This is a contactDetails window</i>
+     * @see contactDetailsTopComponent
+     */
+    static String HINT_contactDetailsTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_contactDetailsTopComponent");
+    }
+    /**
+     * @return <i>This is a contactList window</i>
+     * @see contactListTopComponent
+     */
+    static String HINT_contactListTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_contactListTopComponent");
+    }
+    /**
+     * @return <i>This is a listContact window</i>
+     * @see listContactTopComponent
+     */
+    static String HINT_listContactTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_listContactTopComponent");
+    }
+    /**
+     * @return <i>This is a test window</i>
+     * @see listTopComponent
+     */
+    static String HINT_testTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_testTopComponent");
+    }
+    private Bundle() {}
+}
---
+++ ContactManagerUIModule/src/m1/piu/ContactFormWizardPanel.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m1.piu;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+import org.openide.util.Exceptions;
+import org.openide.util.HelpCtx;
+
+public class ContactFormWizardPanel implements WizardDescriptor.Panel<WizardDescriptor> {
+
+	/**
+	 * The visual component that displays this panel. If you need to access the
+	 * component from this class, just use getComponent().
+	 */
+	//private ContactFormVisualPanel component;
+	public FXMLMainFrameController component;
+
+	// Get the visual component for the panel. In this template, the component
+	// is kept separate. This can be more efficient: if the wizard is created
+	// but never displayed, or not all panels are displayed, it is better to
+	// create only those which really need to be visible.
+	@Override
+	public FXMLMainFrameController getComponent() {
+		if (component == null) {
+			component = new FXMLMainFrameController();
+			Platform.setImplicitExit(false);
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    createScene();  //standard Swing Interop Pattern
+                }
+            });
+		}
+		return component;
+	}
+	
+	private void createScene() {
+        try {
+            URL location = getClass().getResource("FXMLToolBar.fxml"); //same FXML copied from JavaFX app
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(location);
+            fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+            Parent root = (Parent) fxmlLoader.load(location.openStream());
+            Scene scene = new Scene(root);
+            component.setScene(scene);    
+            component = (FXMLMainFrameController) fxmlLoader.getController();    
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+
+	@Override
+	public HelpCtx getHelp() {
+		// Show no Help button for this panel:
+		return HelpCtx.DEFAULT_HELP;
+		// If you have context help:
+		// return new HelpCtx("help.key.here");
+	}
+
+	@Override
+	public boolean isValid() {
+		// If it is always OK to press Next or Finish, then:
+		return true;
+		// If it depends on some condition (form filled out...) and
+		// this condition changes (last form field filled in...) then
+		// use ChangeSupport to implement add/removeChangeListener below.
+		// WizardDescriptor.ERROR/WARNING/INFORMATION_MESSAGE will also be useful.
+	}
+
+	@Override
+	public void addChangeListener(ChangeListener l) {
+	}
+
+	@Override
+	public void removeChangeListener(ChangeListener l) {
+	}
+
+	@Override
+	public void readSettings(WizardDescriptor wiz) {
+		// use wiz.getProperty to retrieve previous panel state
+	}
+
+	@Override
+	public void storeSettings(WizardDescriptor wiz) {
+		// use wiz.putProperty to remember current panel state
+	}
+
+}
